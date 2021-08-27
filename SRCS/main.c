087e3ce75df5e30fa19510811ec8bc13a85b1a2e
aachbaro <aachbaro@student.42.fr>
2021-08-27 16:54:26 +0200

    just for security, ya la window en plus

--- SRCS/main.c
+++ SRCS/main.c
@@ -7,5 +7,6 @@ int	main(int ac, char **av)
 	data.file = ft_strdup(av[ac - 1]);
 	get_map(&data);
 	printf("\nw = %d\nh = %d\n", data.map.w, data.map.h);
+	window(&data);
 	return (0);
 }
--- SRCS/window.c
+++ SRCS/window.c
@@ -12,20 +12,35 @@
 
 #include "../fdf.h"
 
+int	key_press(int keysym, t_mlxdt *data)
+{
+	if (keysym == K_ESCAPE)
+	{
+		mlx_destroy_image(data->m_ptr, data->img.p_img);
+		mlx_destroy_window(data->m_ptr, data->m_win);
+		mlx_destroy_display(data->m_ptr);
+	}
+	return (0);
+}
+
+int	render(t_mlxdt *data)
+{	
+	return (0);
+}
+
 int	window(t_mlxdt *data)
 {
-	int	i;
+	int	x;
+	int	y;
 
-	i = 0;
 	data->m_ptr = mlx_init();
-	data->m_win = mlx_new_window(data->m_ptr, 1920, 1080, "FdF");
-	data->img.p_img = mlx_new_image(data->m_ptr, 1920, 1080);
-	fill_points_tab(&data->map);
+	mlx_get_screen_size(data->m_ptr, &x, &y);
+	data->m_win = mlx_new_window(data->m_ptr, x, y, "FdF");
+	data->img.p_img = mlx_new_image(data->m_ptr, x, y);
 	data->img.addr = mlx_get_data_addr(data->img.p_img, &data->img.bpp,
 			&data->img.line_len, &data->img.endian);
 	mlx_loop_hook(data->m_ptr, &render, data);
-	mlx_hook(data->m_win, 2, (1L<<0), &key_press, &data);	
-	if (data->over == 0)
-		mlx_loop(data->m_ptr);
+	mlx_hook(data->m_win, 2, (1L<<0), &key_press, &data);
+	mlx_loop(data->m_ptr);
 	return (0);
 }
